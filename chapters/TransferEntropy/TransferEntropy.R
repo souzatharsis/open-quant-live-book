e35832eeafa65bcea9c6507021747eb5d5e2d87c
souzatharsis <souza.tharsis@gmail.com>
2019-08-18 22:43:07 -0400

    add code to linear simulated system in Transfer Entropy Chapter

--- chapters/TransferEntropy/TransferEntropy.R
+++ chapters/TransferEntropy/TransferEntropy.R
@@ -9,10 +9,76 @@ library(future)
 FApply.Pairwise <- function(X, D.Func){
   n = seq_len(ncol(X))
 
-  ff.TE.value = function(a, b) calc_te(X[,a], X[,b])
+  ff.TE.value = function(a, b) D.Func(X[,a], X[,b])
 
   return(outer(n, n, Vectorize(ff.TE.value)))
 }
+
+
+Linear.GC <- function(X, Y){
+
+  n<-length(X)
+  X.now<-X[1:(n-1)]
+  Y.now<-Y[1:(n-1)]
+  Y.fut<-Y[2:n]
+
+  regression.uni=lm(Y.fut~Y.now)
+  regression.mult=lm(Y.fut~Y.now+ X.now)
+  var.eps.uni <- (summary(regression.uni)$sigma)^2
+  var.eps.mult <- (summary(regression.mult)$sigma)^2
+  GC <- log(var.eps.uni/var.eps.mult)
+  return(GC)
+}
+
+
+
+
+######## Simulated systems
+
+n.points<-10000
+x1<-x2<-x3<-x4<-x5<-vector()
+x1[1]<-x2[1]<-x3[1]<-x4[1]<-x5[1]<-0
+w1<-rnorm(n.points,0,1)
+w2<-rnorm(n.points,0,1)
+w3<-rnorm(n.points,0,1)
+w4<-rnorm(n.points,0,1)
+w5<-rnorm(n.points,0,1)
+################ LINEAR
+for(i in 2:(n.points)){
+  x1[i] <- 0.95*sqrt(2)*x1[i-1] - 0.9025*x1[i-1] + w1[i]
+  x2[i] <- 0.5*(x1[i-1]) + w2[i]
+  x3[i] <- -0.4*x1[i-1] + w3[i]
+  x4[i] <- -0.5*(x1[i-1]) + 0.25*sqrt(2)*x4[i-1] + 0.25*sqrt(2)*x5[i-1] + w4[i]
+  x5[i] <- -0.25*sqrt(2)*(x4[i-1]) + 0.25*sqrt(2)*x5[i-1] + w5[i]
+}
+x1 <- x1[-1]
+x2 <- x2[-1]
+x3 <- x3[-1]
+x4 <- x4[-1]
+x5 <- x5[-1]
+
+linear.system <- data.frame(x1, x2, x3, x4, x5)
+
+TE.matrix1<-FApply.Pairwise(linear.system, calc_te)
+TE.matrix2<-FApply.Pairwise(linear.system, Linear.GC)
+rownames(TE.matrix1)<-colnames(TE.matrix1)<-var.names<-c("x1", "x2", "x3", "x4", "x5")
+rownames(TE.matrix2)<-colnames(TE.matrix2)<-var.names<-c("x1", "x2", "x3", "x4", "x5")
+corrplot::corrplot(corr = TE.matrix1, diag = FALSE,
+                   is.corr = FALSE, outline = T,tl.col = "black",
+                   method = "color",
+                   col = colorRampPalette(c("blue","white", "red"))(100))
+
+corrplot::corrplot(corr = TE.matrix2, diag = FALSE,
+                   is.corr = FALSE, outline = T,tl.col = "black",
+                   method = "color",
+                   col = colorRampPalette(c("blue","white", "red"))(100))
+
+library(future)
+## Allow for parallel computing
+plan(multiprocess)
+transfer_entropy(x4, x5)
+
+###### Global Market Indices
 
 #https://finance.yahoo.com/world-indices/
 tickers<-c("^GSPC", "^FTSE", "^GDAXI", "^N100", "^BVSP")
